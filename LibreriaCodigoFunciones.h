af76db15011eb20af5ab475e91dc83e821642be8
jsdefrancisco <79968624+jsdefrancisco@users.noreply.github.com>
2022-05-22 01:36:36 +0200

    dos nuevas funciones

--- LibreriaCodigoFunciones.h
+++ LibreriaCodigoFunciones.h
@@ -8,3 +8,5 @@ typedef struct     //estructura para el nombre de la partida/jugador
 
 
 void menu (int *opcion);         //funcion del menu
+void instrucciones (int *informacion);           //funcion de las instruciones
+int  comprobar_fichero_partidas (partida datos[], int *numero_partidas);             //funcion que comprueba si existe la partida seleccionada
