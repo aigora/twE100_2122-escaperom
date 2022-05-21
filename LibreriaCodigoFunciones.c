5096a9a6aa95f6f7d5e7d727c796c7a460f40951
AlvaroVelascoMuela <79969848+AlvaroVelascoMuela@users.noreply.github.com>
2022-05-21 12:18:02 +0200

    subida librería con el código de las funciones

---
+++ LibreriaCodigoFunciones.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include "LibreriaCodigoFunciones.h"
+
+
+void menu (int *opcion)
+{
+
+}
+
+void instrucciones (int *informacion)
+{
+
+}
+
+void nueva_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
+{
+
+}
+
+void cargar_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
+{
+
+}
+
+int  comprobar_fichero_partidas (partida datos[], int *numero_partidas)
+{
+
+}
