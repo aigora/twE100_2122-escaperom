30e3730c8e5f1fa9f9b2ca5ada9916c812f8a7b2
jsdefrancisco <79968624+jsdefrancisco@users.noreply.github.com>
2022-05-22 01:42:19 +0200

    ordenacion de las funciones

--- LibreriaCodigoFunciones.c
+++ LibreriaCodigoFunciones.c
@@ -33,16 +33,6 @@ void instrucciones (int *informacion)
 
 }
 
-void nueva_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
-{
-
-}
-
-void cargar_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
-{
-
-}
-
 int  comprobar_fichero_partidas (partida datos[], int *numero_partidas)
 {
 	FILE * pv ;   //p hay que ponerle nombre correspondiente
@@ -65,4 +55,14 @@ int  comprobar_fichero_partidas (partida datos[], int *numero_partidas)
 
         fclose(pv);
         return 1;
+}
+
+void nueva_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
+{
+
+}
+
+void cargar_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
+{
+
 }
