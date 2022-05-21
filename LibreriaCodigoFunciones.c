e1a98378f1abed61ebd4fd9e5b2cf05c8f3eb4b4
GuillermoQueroPonce <79969752+GuillermoQueroPonce@users.noreply.github.com>
2022-05-21 19:51:37 +0200

    He añadido la función menú

--- LibreriaCodigoFunciones.c
+++ LibreriaCodigoFunciones.c
@@ -5,7 +5,20 @@
 
 void menu (int *opcion)
 {
+  system("cls");  //Borrar la pantalla  para poder mirar cuando se quiera las instruciones.
 
+	printf("Bienvenido a EscapeROM\n");
+
+	printf("Selecciona la opcion:\n");
+
+	printf("Nueva partida   1\n");
+	printf("Cargar partida  2\n");
+	printf("Intrucciones    3\n");
+	printf("Salir           4\n");
+
+	fflush(stdin);
+
+	scanf("%i", &*opcion);
 }
 
 void instrucciones (int *informacion)
