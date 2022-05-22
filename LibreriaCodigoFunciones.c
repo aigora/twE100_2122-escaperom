e27fa6acabf0c4d734eb050a397bf67bceb5f6b8
MarioTorre <79970054+MarioTorre@users.noreply.github.com>
2022-05-22 18:55:28 +0200

    Update LibreriaCodigoFunciones.c

--- LibreriaCodigoFunciones.c
+++ LibreriaCodigoFunciones.c
@@ -58,11 +58,95 @@ int  comprobar_fichero_partidas (partida datos[], int *numero_partidas)
 }
 
 void nueva_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
-{
+{ //printf("sigue las instrucciones para crear la partida\n");
+
+    FILE * pv; //poner nombre correspondiente
+    int i, aux = 0;
+
+    do          //se comprueba que el nombre de la partida es el correcto y no existe otro igual
+    {
+        printf("nombre de la partida\n");
+
+        fflush(stdin);
+
+        scanf("%s", nombre_jugador);
+
+        for(i = 0; i < *numero_partidas; i++)
+        {
+			if(strcmp(nombre_jugador, datos[i].nombre_partida) == 0)
+			{
+				aux = 1;
+				break;
+			}
+            else
+            {
+				aux = 0;
+			}
+		}
+
+        if(aux == 1)
+        {
+			printf("el nombre de la partida ya existe, inserte otro nombre\n");
+			system("PAUSE");
+		}
+		else
+        {
+			printf("nombre de la partida guardado correctamente\n");
+			strcpy(datos[*numero_partidas].nombre_partida, nombre_jugador);
+		}
+    }
+    while(aux == 1);
+
+    pv = fopen("NOMBRES_PARTIDA.txt", "w");
+
+    for(i = 0; i <= *numero_partidas; i++)          //se añaden en el fichero los datos introducidos
+    {
+        fprintf(pv, "%s\n", datos[i].nombre_partida);
+    }
+
+    fclose(pv);
+    system("PAUSE");
 
 }
 
 void cargar_partida (partida datos[], char nombre_jugador[], int *numero_partidas)
-{
+{int i, aux = 0;
+
+    printf("cargar partida\n");
+
+    do              //introduce el nombre de la partida y
+    {
+        printf("nombre de la partida\n");
+        fflush(stdin);
+        scanf("%s",nombre_jugador);
+
+        for(i = 0; i < *numero_partidas; i++)           //comprobamos que el nombre de la partida existe
+        {
+            if(strcmp(nombre_jugador, datos[i].nombre_partida) == 0)
+            {
+                aux = 1;
+                break;
+            }
+            else
+            {
+                aux = 0;
+            }
+        }
+
+        if(aux == 0)
+        {
+            printf("nombre de la partida incorrecto\n");
+            printf("vuelve a introducirlo\n");
+        }
+        else
+        {
+            printf("partida cargada correctamente\n");
+        }
+
+        system("PAUSE");
+
+    }
+    while(aux == 0);            //mientras aux valga 0 se hace el bucle y cuando cambia deja de preguntar
+}
 
 }
