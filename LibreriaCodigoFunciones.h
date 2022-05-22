95fb419ff330a57763826ef4a7cbedd122d6d993
MarioTorre <79970054+MarioTorre@users.noreply.github.com>
2022-05-22 18:59:08 +0200

    Update LibreriaCodigoFunciones.h

--- LibreriaCodigoFunciones.h
+++ LibreriaCodigoFunciones.h
@@ -9,4 +9,6 @@ typedef struct     //estructura para el nombre de la partida/jugador
 
 void menu (int *opcion);         //funcion del menu
 void instrucciones (int *informacion);           //funcion de las instruciones
+void nueva_partida (partida datos[], char nombre_jugador[], int *numero_partidas);            //funcion que crea una partida nueva
+void cargar_partida (partida datos[], char nombre_jugador[], int *numero_partidas);           //funcion que carga partida seleccionada
 int  comprobar_fichero_partidas (partida datos[], int *numero_partidas);             //funcion que comprueba si existe la partida seleccionada
